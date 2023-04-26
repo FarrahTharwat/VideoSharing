bea549b4c0e91baf6bef7b87e77656febec96459
omarsorourr <omarsorour.os@gmail.com>
2023-04-26 17:48:04 +0200

    create crud interface modify database class

---
+++ Model/CRUD.php
@@ -0,0 +1,10 @@
+<?php
+interface CRUD{
+
+    public function create($entity) ;
+    public function delete($id);
+    public function retrive($id);
+    public function update ($entity);
+
+
+}
--- Model/Database.php
+++ Model/Database.php
@@ -7,10 +7,15 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'video_sharing');
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($conn->connect_error) {
-    die('Connection Failed' . $conn->connect_error);
+
+
+
+function conection()
+{
+    $conn = new mysqli(DB_Host, DB_User, DB_PASS, DB_NAME);
+    if ($conn->connect_error) {
+        die('Connection Failed' . $conn->connect_error);
+    }
+  return $conn;
 }
-
-
 ?>
