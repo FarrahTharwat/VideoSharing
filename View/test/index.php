b934bf7641707d8a5e72b27a719cc6d5b99bdc24
FarrahTharwat <105726046+FarrahTharwat@users.noreply.github.com>
2023-05-09 00:24:30 +0300

    Add files via upload

---
+++ View/test/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>AJAX Test</title>
+    <script src="ajax.js"></script>
+</head>
+
+<body>
+    <button onclick="triggerWowFunction()">Trigger Wow Function</button>
+</body>
+
+
+</html>
