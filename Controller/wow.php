1063c4de7a868896ee0feabba69a5b191daa3d4f
FarrahTharwat <105726046+FarrahTharwat@users.noreply.github.com>
2023-05-08 23:58:08 +0300

    //yarab Astoria

--- Controller/wow.php
+++ Controller/wow.php
@@ -1,22 +1,16 @@
-
 <?php
-include 'ManageHistory.php';
-if (isset($_POST['action']) && $_POST['action'] == 'wow') {
-    // Call the wow function and echo a message
-wow($_POST['userID'], $_POST['videoID']);
-echo "Wow, the wow function was called with name ".$_POST['userID']." and age ".$_POST['videoID']."!";
+include 'ManageReact.php';
+ echo $_POST['userID'];
+if (isset($_POST['action']) && $_POST['action'] == 'addReacts') {
+    // Call the addReact function and echo a message
+
+    addReacts($_POST['userID'], $_POST['videoID'], $_POST['react']);
+    echo "React added successfully";
+    echo"wow";
 }
 
-
-function wow($userID, $videoID)
+function addReacts($userID, $videoID, $react)
 {
-    $kk = new ManageHistory();
-    $kk->remove($userID,$videoID);
-    echo "wooooooooow";
+    $manage = new ManageReact();
+    $manage->addReact($videoID, $userID, $react);
 }
-
-?>
-
-
-
-
