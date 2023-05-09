93ffb844ce290f95c5bf3f5c964c24f50e40f505
Farrah Tharwat <105726046+FarrahTharwat@users.noreply.github.com>
2023-05-09 15:45:51 +0300

    Revert "Revert "///HAMOT YANAS""
    This reverts commit 0ccfee0708f6ef90a0e1c0f49aceb5e979aaf67a.

--- Controller/ManageSubscribtion.php
+++ Controller/ManageSubscribtion.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 include 'Database.php';
 class ManageSubscribtion
@@ -64,40 +63,3 @@ class ManageSubscribtion
         $conn->close();
     }
 }
-=======
-<?php
-require_once 'Database.php';
-class ManageSubscribtion
-{
-    public function addsubscription($SubscriberID,$ChannelID)
-    {
-        $database = new Database();
-        $conn = $database->getConn();
-
-        $query ="INSERT INTO subscribed values ('$SubscriberID','$ChannelID',0)";
-        if ($conn->query($query) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $query . "<br>" . $conn->error;
-        }
-
-// Close the database connection
-        $conn->close();
-    }
-    public function deletesubscription($SubscriberID,$ChannelID)
-    {
-        $database = new Database();
-        $conn = $database->getConn();
-
-        $query ="DELETE FROM subscribed WHERE SubscriberID='$SubscriberID' and ChannelID='$ChannelID'";
-        if ($conn->query($query) === TRUE) {
-            echo "record Deleted successfully";
-        } else {
-            echo "Error: " . $query . "<br>" . $conn->error;
-        }
-
-// Close the database connection
-        $conn->close();
-    }
-}
->>>>>>> Stashed changes
