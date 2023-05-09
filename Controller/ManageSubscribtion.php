bc26365bdda7eacc5058210f5df04ca1a16b1cb3
omaremad2 <103078265+omaremad2@users.noreply.github.com>
2023-05-09 13:27:49 +0300

    .....

--- Controller/ManageSubscribtion.php
+++ Controller/ManageSubscribtion.php
@@ -2,6 +2,36 @@
 include 'Database.php';
 class ManageSubscribtion
 {
+    public function addsubscription($SubscriberID,$ChannelID)
+    {
+        $database = new Database();
+        $conn = $database->getConn();
+
+        $query ="INSERT INTO subscribed values ('$SubscriberID','$ChannelID',0)";
+        if ($conn->query($query) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
+
+// Close the database connection
+        $conn->close();
+    }
+    public function deletesubscription($SubscriberID,$ChannelID)
+    {
+        $database = new Database();
+        $conn = $database->getConn();
+
+        $query ="DELETE FROM subscribed WHERE SubscriberID='$SubscriberID' and ChannelID='$ChannelID'";
+        if ($conn->query($query) === TRUE) {
+            echo "record Deleted successfully";
+        } else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
+
+// Close the database connection
+        $conn->close();
+    }
     public function toggleSubscription($subscriberID, $channelID)
     {
         $database = new Database();
