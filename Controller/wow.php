d0230be001c11dc88523f15375fde4cf4b989174
Farrah Tharwat <105726046+FarrahTharwat@users.noreply.github.com>
2023-05-09 15:40:30 +0300

    ///HAMOT YANAS

--- Controller/wow.php
+++ Controller/wow.php
@@ -1,23 +1,8 @@
-<<<<<<< Updated upstream
 <?php
-include 'ManageReact.php';
-include 'ManageHistory.php';
- echo $_POST['userID'];
-if (isset($_POST['action']) && $_POST['action'] == 'addReacts') {
-    // Call the addReact function and echo a message
 
-    addReacts($_POST['userID'], $_POST['videoID'], $_POST['react']);
-    echo "React added successfully";
-    echo"wow";
-}
-
-function addReacts($userID, $videoID, $react)
-{
-    $manage = new ManageReact();
-    $manage->addReact($videoID, $userID, $react);
-}
- echo $_POST['userID'];
-
+require_once 'ManageReact.php';
+require_once 'ManageHistory.php';
+require_once 'ManageSubscribtion.php';
 
 
  if (isset($_POST['action']) && $_POST['action'] == 'wow') {
@@ -50,13 +35,7 @@ function addReacts($userID, $videoID, $react)
      $kk->delete($userID);
      echo "wooooooooow";
  }
- ?>
-=======
-<?php
-require_once 'Database.php';
-require_once 'ManageReact.php';
-require_once 'ManageHistory.php';
-require_once 'ManageSubscribtion.php';
+///////////
 
 if (isset($_POST['action']) && $_POST['action'] == 'addReacts') {
     // Call the addReact function and echo a message
@@ -72,36 +51,6 @@ function addReacts($userID, $videoID, $react)
     $manage->addReact($videoID, $userID, $react);
 }
 
-if (isset($_POST['action']) && $_POST['action'] == 'wow') {
-    // Call the wow function and echo a message
-    wow($_POST['userID'], $_POST['videoID']);
-
-}
-
-
-function wow($userID, $videoID)
-{
-    $kk = new ManageHistory();
-    $kk->remove($userID,$videoID);
-
-}
-
-
-
-
-if (isset($_POST['action']) && $_POST['action'] == 'weew') {
-    // Call the wow function and echo a message
-    weew($_POST['userID']);
-
-}
-
-
-function weew($userID)
-{
-    $kk = new ManageHistory();
-    $kk->delete($userID);
-
-}
 if (isset($_POST['action']) && $_POST['action'] == 'subscribe') {
     // Call the addReact function and echo a message
 
@@ -113,4 +62,4 @@ function toggleSubscribe($userID,$channelID)
     $manage = new  ManageSubscribtion();
     $manage->addsubscription($userID,$channelID);
 }
->>>>>>> Stashed changes
+
